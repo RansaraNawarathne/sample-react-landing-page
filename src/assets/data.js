103e7234cf4fd6bda421b8e2ed22eb3bb6290feb
Malindu Nawarathne <ransaranawarathne@gmail.com>
2024-09-16 19:24:37 +0530

    Feat: added constant values

---
+++ src/assets/data.js
@@ -0,0 +1,10 @@
+export const HERO_SECTION_DATA = {
+    heading: 'Introduce Your Product Quickly & Effectively',
+    body: ['Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus ', `mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.`]
+}
+
+export const HERO_SECTION_BUTTONS = Object.freeze({
+    BUY_NOW: 'Buy Now',
+    LEARN_MORE: 'Learn More',
+    PURCHASE_UI_KIT: 'Purchase UI Kit'
+})
